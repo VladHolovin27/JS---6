1605777781da381b9dd4b7e410a35451323d033c
VladHolovin27 <105449427+VladHolovin27@users.noreply.github.com>
2023-03-03 17:32:56 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+// Exercise 1
+
+// let b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < b.length; i++) {
+//   if (b[i] <= 5) {
+//     console.log(b[i]);
+//   }
+// }
+
+// Exercise 2
+
+let be = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 5; i < be.length; i++) {
+  if (be[i] > 5 && be[i] < 8) {
+    console.log(be[i]);
+  }
+}
